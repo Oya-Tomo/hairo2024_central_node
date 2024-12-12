258aa271336f61b389d8c8463ed3b0183b9869ff
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-12 14:32:08 +0900

    update: add state sender

--- nodes/left_footer.py
+++ nodes/left_footer.py
@@ -1,6 +1,55 @@
+import math
+import pigpio
+
+from utils import map_to_int8t, int_to_uint8t
+
+LEFT_FOOTER_I2C_ADDR = 0x10
+
 IS_RUNNING_BYTE = 0x00
 
 SPEED_BYTE = 0x01
 
 FRONT_ANGLE_BYTE = 0x02
 BACK_ANGLE_BYTE = 0x03
+
+
+def send_state(
+    pi: pigpio.pi,
+    is_running: bool,
+    speed: float,
+    front_angle: float,
+    back_angle: float,
+):
+    handle = pi.i2c_open(1, LEFT_FOOTER_I2C_ADDR)
+
+    pi.i2c_write_byte(handle, [IS_RUNNING_BYTE, int(is_running)])
+    pi.i2c_write_byte(
+        handle, [SPEED_BYTE, int_to_uint8t(map_to_int8t(speed, -1.0, 1.0))]
+    )
+    pi.i2c_write_byte(
+        handle,
+        [
+            FRONT_ANGLE_BYTE,
+            int_to_uint8t(
+                map_to_int8t(
+                    front_angle,
+                    -math.pi / 2,
+                    math.pi / 2,
+                ),
+            ),
+        ],
+    )
+    pi.i2c_write_byte(
+        handle,
+        [
+            BACK_ANGLE_BYTE,
+            int_to_uint8t(
+                map_to_int8t(
+                    back_angle,
+                    -math.pi / 2,
+                    math.pi / 2,
+                ),
+            ),
+        ],
+    )
+    pi.i2c_close(handle)
