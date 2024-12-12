5a49d461e87c03cfce1f6245d81672cc2d4eb044
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-12 13:52:13 +0900

    update: add timeout to connection loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 import pprint
 
-from state import unpack_state
+from state import unpack_state, SystemState, FooterState, ArmState, CollectionState
 
 
 PORT = 5000
@@ -12,30 +12,58 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 server.listen()
 
+system_state: SystemState = SystemState(
+    is_running=False,
+)
+footer_state: FooterState = FooterState(
+    left_speed=0.0,
+    right_speed=0.0,
+    left_front_flipper=0.0,
+    left_back_flipper=0.0,
+    right_front_flipper=0.0,
+    right_back_flipper=0.0,
+)
+arm_state: ArmState = ArmState(
+    base_angle=0.0,
+    mid_angle=0.0,
+    tip_angle=0.0,
+    rotate=0.0,
+    gripper_speed=0.0,
+)
+collection_state: CollectionState = CollectionState(
+    speed=0.0,
+    angle=0.0,
+)
 
 try:
     while True:
-        client, addr = server.accept()
-        data = client.recv(1024)
+        try:
+            server.settimeout(1)
+            client, addr = server.accept()
+            data = client.recv(1024)
 
-        if data == b"":  # Disconnected
+            if data == b"":  # Disconnected
+                client.close()
+                continue
+
+            system_state, footer_state, arm_state, collection_state = unpack_state(data)
+
+            pprint.pprint(system_state)
+            pprint.pprint(footer_state)
+            pprint.pprint(arm_state)
+            pprint.pprint(collection_state)
+
+            client.sendall("ok".encode("utf-8"))
             client.close()
-            continue
+            server.settimeout(None)
+        except socket.timeout:
+            print("timeout")
 
-        system_state, footer_state, arm_state, collection_state = unpack_state(data)
-
-        pprint.pprint(system_state)
-        pprint.pprint(footer_state)
-        pprint.pprint(arm_state)
-        pprint.pprint(collection_state)
-
-        client.sendall("ok".encode("utf-8"))
-        client.close()
 
 except KeyboardInterrupt as e:
     print("Finished!")
-    server.close()
 
 finally:
+    server.shutdown(socket.SHUT_RDWR)
     server.close()
     print("Server closed!")
