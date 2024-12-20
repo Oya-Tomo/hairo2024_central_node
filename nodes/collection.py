439aabe725608aa31a0d6f3ff65f674520b777ea
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-20 19:44:50 +0900

    update: add auto detection

--- nodes/collection.py
+++ nodes/collection.py
@@ -1,5 +1,4 @@
 import math
-from typing import Any
 import pigpio
 
 from utils import map_to_int8t, int_to_uint8t
@@ -17,18 +16,21 @@ GRIPPER_SPEED_BYTE = 0x05
 BELT_SPEED_BYTE = 0x06
 COLLECTOR_ANGLE_BYTE = 0x07
 
+handle = None
+
 
 def get_handle(pi: pigpio.pi):
     return pi.i2c_open(1, COLLECTION_I2C_ADDR)
 
 
-def close_handle(pi: pigpio.pi, handle: Any):
-    pi.i2c_close(handle)
+def close_handle(pi: pigpio.pi):
+    if handle != None:
+        pi.i2c_close(handle)
+        handle = None
 
 
 def send_state(
     pi: pigpio.pi,
-    handle: Any,
     is_running: bool,
     base_angle: float,
     mid_angle: float,
@@ -37,41 +39,50 @@ def send_state(
     gripper_speed: float,
     belt_speed: float,
     angle: float,
-):
-    pi.i2c_write_byte_data(handle, IS_RUNNING_BYTE, int(is_running))
+) -> bool:
+    global handle
+    try:
+        if handle == None:
+            handle = get_handle(pi)
 
-    pi.i2c_write_byte_data(
-        handle,
-        BASE_ANGLE_BYTE,
-        int_to_uint8t(map_to_int8t(base_angle, -math.pi, math.pi)),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        MID_ANGLE_BYTE,
-        int_to_uint8t(map_to_int8t(mid_angle, -math.pi, math.pi)),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        TIP_ANGLE_BYTE,
-        int_to_uint8t(map_to_int8t(tip_angle, -math.pi, math.pi)),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        ROTATE_BYTE,
-        int_to_uint8t(map_to_int8t(rotate, -math.pi, math.pi)),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        GRIPPER_SPEED_BYTE,
-        int_to_uint8t(map_to_int8t(gripper_speed, -1.0, 1.0)),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        BELT_SPEED_BYTE,
-        int_to_uint8t(map_to_int8t(belt_speed, -1.0, 1.0)),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        COLLECTOR_ANGLE_BYTE,
-        int_to_uint8t(map_to_int8t(angle, -math.pi / 4, math.pi / 4)),
-    )
+        pi.i2c_write_byte_data(handle, IS_RUNNING_BYTE, int(is_running))
+
+        pi.i2c_write_byte_data(
+            handle,
+            BASE_ANGLE_BYTE,
+            int_to_uint8t(map_to_int8t(base_angle, -math.pi, math.pi)),
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            MID_ANGLE_BYTE,
+            int_to_uint8t(map_to_int8t(mid_angle, -math.pi, math.pi)),
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            TIP_ANGLE_BYTE,
+            int_to_uint8t(map_to_int8t(tip_angle, -math.pi, math.pi)),
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            ROTATE_BYTE,
+            int_to_uint8t(map_to_int8t(rotate, -math.pi, math.pi)),
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            GRIPPER_SPEED_BYTE,
+            int_to_uint8t(map_to_int8t(gripper_speed, -1.0, 1.0)),
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            BELT_SPEED_BYTE,
+            int_to_uint8t(map_to_int8t(belt_speed, -1.0, 1.0)),
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            COLLECTOR_ANGLE_BYTE,
+            int_to_uint8t(map_to_int8t(angle, -math.pi / 4, math.pi / 4)),
+        )
+        return True
+    except Exception as e:
+        handle = None
+        return False
--- nodes/right_footer.py
+++ nodes/right_footer.py
@@ -13,46 +13,59 @@ SPEED_BYTE = 0x01
 FRONT_ANGLE_BYTE = 0x02
 BACK_ANGLE_BYTE = 0x03
 
+handle = None
+
 
 def get_handle(pi: pigpio.pi):
     return pi.i2c_open(1, RIGHT_FOOTER_I2C_ADDR)
 
 
-def close_handle(pi: pigpio.pi, handle: Any):
-    pi.i2c_close(handle)
+def close_handle(pi: pigpio.pi):
+    global handle
+    if handle != None:
+        pi.i2c_close(handle)
+        handle = None
 
 
 def send_state(
     pi: pigpio.pi,
-    handle: Any,
     is_running: bool,
     speed: float,
     front_angle: float,
     back_angle: float,
 ):
-    pi.i2c_write_byte_data(handle, IS_RUNNING_BYTE, int(is_running))
-    pi.i2c_write_byte_data(
-        handle, SPEED_BYTE, int_to_uint8t(map_to_int8t(speed, -1.0, 1.0))
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        FRONT_ANGLE_BYTE,
-        int_to_uint8t(
-            map_to_int8t(
-                front_angle,
-                -math.pi / 2,
-                math.pi / 2,
+    global handle
+    try:
+        if handle == None:
+            handle = get_handle(pi)
+
+        pi.i2c_write_byte_data(handle, IS_RUNNING_BYTE, int(is_running))
+        pi.i2c_write_byte_data(
+            handle, SPEED_BYTE, int_to_uint8t(map_to_int8t(speed, -1.0, 1.0))
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            FRONT_ANGLE_BYTE,
+            int_to_uint8t(
+                map_to_int8t(
+                    front_angle,
+                    -math.pi / 2,
+                    math.pi / 2,
+                ),
             ),
-        ),
-    )
-    pi.i2c_write_byte_data(
-        handle,
-        BACK_ANGLE_BYTE,
-        int_to_uint8t(
-            map_to_int8t(
-                back_angle,
-                -math.pi / 2,
-                math.pi / 2,
+        )
+        pi.i2c_write_byte_data(
+            handle,
+            BACK_ANGLE_BYTE,
+            int_to_uint8t(
+                map_to_int8t(
+                    back_angle,
+                    -math.pi / 2,
+                    math.pi / 2,
+                ),
             ),
-        ),
-    )
+        )
+        return True
+    except Exception as e:
+        handle = None
+        return False
